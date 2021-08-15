dba5a57a56392b7ad94a33a311c4acf0ab02d127
launchpad5682 <launchpad5682@gmail.com>
2021-08-15 23:26:24 +0530

    resource updated

--- src/resource.js
+++ src/resource.js
@@ -1,9 +1,12 @@
 export const emojiDictionary = {
-    "😀": "Grinning Face",
-    "🤔": "Thinking Face",
-    "🤮": "Face with open mouth vomiting",
-    "🥱": "Yawning Face",
-    "😤": "Angry Face",
-    "😫": "Tired Face",
-    "😘": "Face throwing a kiss",
-}
+  "😀": "Grinning Face",
+  "🤔": "Thinking Face",
+  "🤮": "Face with open mouth vomiting",
+  "🥱": "Yawning Face",
+  "😤": "Angry Face",
+  "😫": "Tired Face",
+  "😘": "Face throwing a kiss",
+  "😵": "Dizzy Face",
+  "😖": "Confounded Face",
+  "😂": "Face with Tears of Joy",
+};
